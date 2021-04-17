5d465672dc79c57f1a55bbfe69307d19a2bd1ea7
deikare <bbgolawski@gmail.com>
2021-04-18 00:54:24 +0200

    Bad idea with certs debunked

--- infrastructure/kafka-generate-certs.sh
+++ infrastructure/kafka-generate-certs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CERT_PATH="certs"
+CA_PATH="certs"
 KAFKA_PATH="certs/kafka"
 
 rm $KAFKA_PATH/*
@@ -12,39 +12,75 @@ ORGANIZATION="Warsaw University of Technology"
 ORGANIZATION_UNIT="Warsaw University of Technology"
 
 ALIAS="kafka"
-SAN="kafka"
+SAN="DNS:localhost"
 CN="kafka"
 VALIDITY="365"
 KEYALG="RSA"
+
+STOREPASS="confluent"
+KEYPASS="confluent"
 
 NODES_NUMBER=3
 
 for ((i=1;i<=NODES_NUMBER;i++)); do
     CN_ITERATION=$CN$i
-    SAN_ITERATION=$SAN$i
+
+    STOREPASS_ITERATION="confluent"${i}
+    KEYPASS_ITERATION="confluent"${i}
+
+    # Create .conf file for proper openssl signing
+    echo "[req]" > $KAFKA_PATH/server$i.conf
+    echo "distinguished_name = req_distinguished_name" >> $KAFKA_PATH/server$i.conf
+    echo "req_extensions = v3_req" >> $KAFKA_PATH/server$i.conf
+    echo "prompt = no" >> $KAFKA_PATH/server$i.conf
+    echo -e "" >> $KAFKA_PATH/server$i.conf
+
+    echo "[req_distinguished_name]" >> $KAFKA_PATH/server$i.conf
+    echo "C = $COUNTRY" >> $KAFKA_PATH/server$i.conf
+    echo "ST = $STATE" >> $KAFKA_PATH/server$i.conf
+    echo "L = $LOCATION" >> $KAFKA_PATH/server$i.conf
+    echo "O = $ORGANIZATION" >> $KAFKA_PATH/server$i.conf
+    echo "prompt = no" >> $KAFKA_PATH/server$i.conf
+    echo "CN = $CN" >> $KAFKA_PATH/server$i.conf
+    echo -e "" >> $KAFKA_PATH/server$i.conf
+
+    echo "[v3_req]" >> $KAFKA_PATH/server$i.conf
+    echo "keyUsage = keyEncipherment, dataEncipherment" >> $KAFKA_PATH/server$i.conf
+    echo "extendedKeyUsage = serverAuth" >> $KAFKA_PATH/server$i.conf
+    echo "subjectAltName = $SAN" >> $KAFKA_PATH/server$i.conf
+    echo "[alt_names]" >> $KAFKA_PATH/server$i.conf
+    echo "DNS.1 = $CN_ITERATION" >> $KAFKA_PATH/server$i.conf
+    echo "DNS.2 = localhost" >> $KAFKA_PATH/server$i.conf
+
     # Create keystore
     keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias $ALIAS$i -validity $VALIDITY -genkey -keyalg $KEYALG \
-        -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$ORGANIZATION_UNIT, CN=$CN_ITERATION" -ext SAN=DNS:$SAN_ITERATION \
-        -noprompt \
-        -storepass confluent -keypass confluent
+        -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$ORGANIZATION, CN=$CN_ITERATION" -ext SAN=$SAN \
+        -storepass $STOREPASS_ITERATION -keypass $KEYPASS_ITERATION
         
 
-    # Generate csr
-    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias $ALIAS$i -certreq -file $KAFKA_PATH/kafka$i.crt -storepass confluent -keypass confluent
+    # Generate csr # - keytool nie generuje SANów
+    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias $ALIAS$i -certreq -file $KAFKA_PATH/kafka$i.csr -ext SAN=$SAN \
+        -storepass $STOREPASS_ITERATION -keypass $KEYPASS_ITERATION 
+
+    openssl req -in $KAFKA_PATH/kafka$i.csr -noout -text 
 
     # Sign crt
-    openssl x509 -req -CA $CERT_PATH/ca.crt -CAkey $CERT_PATH/ca.key -in $KAFKA_PATH/kafka$i.crt -out $KAFKA_PATH/kafka$i.signed.crt -days $VALIDITY -CAcreateserial  
+    openssl x509 -req -CA $CA_PATH/ca.crt -CAkey $CA_PATH/ca.key -in $KAFKA_PATH/kafka$i.csr -out $KAFKA_PATH/kafka$i.signed.crt -days $VALIDITY -CAcreateserial  \
+        -extfile $KAFKA_PATH/server$i.conf -extensions v3_req
+
+    openssl x509 -in $KAFKA_PATH/kafka$i.signed.crt -noout -text 
 
     # Add CA to keystore
-    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias CAroot -import -file $CERT_PATH/ca.crt -storepass confluent -keypass confluent
+    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias CAroot -import -file $CA_PATH/ca.crt -storepass $STOREPASS_ITERATION -keypass $KEYPASS_ITERATION
 
     # Add signed cert to keystore
-    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias $ALIAS$i -import -file $KAFKA_PATH/kafka$i.signed.crt -storepass confluent -keypass confluent
+    keytool -keystore $KAFKA_PATH/kafka.broker$i.keystore.jks -alias $ALIAS$i -import -file $KAFKA_PATH/kafka$i.signed.crt -ext SAN=$SAN \
+        -storepass $STOREPASS_ITERATION -keypass $KEYPASS_ITERATION
 
     # Add CA to truststore
-    keytool -keystore $KAFKA_PATH/kafka.broker$i.truststore.jks -alias CAroot -importcert -file $CERT_PATH/ca.crt -storepass confluent -keypass confluent
+    keytool -keystore $KAFKA_PATH/kafka.broker$i.truststore.jks -alias CAroot -importcert -file $CA_PATH/ca.crt -storepass $STOREPASS_ITERATION -keypass $KEYPASS_ITERATION
 
-    echo "confluent" > $KAFKA_PATH/${i}_sslkey_creds        
-    echo "confluent" > $KAFKA_PATH/${i}_keystore_creds
-    echo "confluent" > $KAFKA_PATH/${i}_truststore_creds
+    echo "confluent${i}" > $KAFKA_PATH/${i}_sslkey_creds        
+    echo "confluent${i}" > $KAFKA_PATH/${i}_keystore_creds
+    echo "confluent${i}" > $KAFKA_PATH/${i}_truststore_creds
 done
