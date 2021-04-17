773a5744d83ce6201b59c7ebf4d5f5d41ff99ca5
deikare <bbgolawski@gmail.com>
2021-04-17 02:04:29 +0200

    Test

--- infrastructure/kafka-generate-certs.sh
+++ infrastructure/kafka-generate-certs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-PATH="certs/kafka"
+path="certs"
+
+rm $path/kafka/*
 
 COUNTRY="PL"
 STATE="Mazowieckie"
@@ -16,8 +18,23 @@ KEYALG="RSA"
 NODES_NUMBER=3
 
 for ((i=1;i<=NODES_NUMBER;i++)); do
-    /usr/bin/keytool -keystore $PATH/kafka.server$i.keystore.jks -alias $ALIAS$i -validity $VALIDITY -genkey -keyalg $KEYALG -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$ORGANIZATION_UNIT, CN=kafka$i" -ext SAN=DNS:$SAN$i
-    /usr/bin/keytool -list -v -keystore $PATH/kafka.server$i.keystore.jks
-    /usr/bin/keytool -keystore kafka.server.truststore.jks -alias CA -importcert -file $PATH/ca.crt
-    /usr/bin/keytool -keystore kafka.server.keystore.jks -alias $ALIAS$i -certreq -file cert-file
+    #echo $'\nCreate keystores'
+    keytool -keystore $path/kafka/kafka.server$i.keystore.jks -alias $ALIAS$i -validity $VALIDITY -genkey -keyalg $KEYALG \
+        -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$ORGANIZATION_UNIT, CN=kafka$i" -ext SAN=DNS:$SAN$i \
+        -storepass confluent -keypass confluent
+
+    #echo "Generate csr"
+    keytool -keystore $path/kafka/kafka.server$i.keystore.jks -alias $ALIAS$i -certreq -file $path/kafka/kafka$i.crt -storepass confluent -keypass confluent
+
+    #echo "Sign crt"
+    openssl x509 -req -CA $path/ca.crt -CAkey $path/ca.key -in $path/kafka/kafka$i.crt -out $path/kafka/kafka$i.signed.crt -days $VALIDITY -CAcreateserial 
+
+    #echo "Add CA to keystore"
+    keytool -keystore $path/kafka.server$i.keystore.jks -alias CA -import -file $path/ca.crt -storepass confluent -keypass confluent
+
+    #echo "Add signed cert to keystore"
+    keytool -keystore $path/kafka.server$i.keystore.jks -alias $path/kafka/kafka$i.crt -import -file $path/kafka/kafka$i.signed.crt -storepass confluent -keypass confluent
+
+    #echo "Add CA to truststore"
+    keytool -keystore $path/kafka/kafka.server$i.truststore.jks -alias CA -importcert -file $path/ca.crt -storepass confluent -keypass confluent
 done
