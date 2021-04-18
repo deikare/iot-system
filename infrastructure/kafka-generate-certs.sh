cc9efa0534279036c5009c097be2ce70c0939d49
deikare <bbgolawski@gmail.com>
2021-04-18 03:53:01 +0200

    Changes to revert

--- infrastructure/kafka-generate-certs.sh
+++ infrastructure/kafka-generate-certs.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 CA_PATH="certs"
 KAFKA_PATH="certs/kafka"
+=======
+path="certs"
+>>>>>>> parent of 23e6b25... Kafka is almost done, only custom hosts are needed
 
-rm $KAFKA_PATH/*
+rm $path/kafka/*
 
 COUNTRY="PL"
 STATE="Mazowieckie"
@@ -12,8 +16,12 @@ ORGANIZATION="Warsaw University of Technology"
 ORGANIZATION_UNIT="Warsaw University of Technology"
 
 ALIAS="kafka"
+<<<<<<< HEAD
 SAN="DNS:localhost"
 CN="kafka"
+=======
+SAN="kafka"
+>>>>>>> parent of 23e6b25... Kafka is almost done, only custom hosts are needed
 VALIDITY="365"
 KEYALG="RSA"
 
@@ -23,6 +31,7 @@ KEYPASS="confluent"
 NODES_NUMBER=3
 
 for ((i=1;i<=NODES_NUMBER;i++)); do
+<<<<<<< HEAD
     CN_ITERATION=$CN$i
 
     STOREPASS_ITERATION="confluent"${i}
@@ -83,4 +92,25 @@ for ((i=1;i<=NODES_NUMBER;i++)); do
     echo "confluent${i}" > $KAFKA_PATH/${i}_sslkey_creds        
     echo "confluent${i}" > $KAFKA_PATH/${i}_keystore_creds
     echo "confluent${i}" > $KAFKA_PATH/${i}_truststore_creds
+=======
+    #echo $'\nCreate keystores'
+    keytool -keystore $path/kafka/kafka.server$i.keystore.jks -alias $ALIAS$i -validity $VALIDITY -genkey -keyalg $KEYALG \
+        -dname "C=$COUNTRY, ST=$STATE, L=$LOCATION, O=$ORGANIZATION_UNIT, CN=kafka$i" -ext SAN=DNS:$SAN$i \
+        -storepass confluent -keypass confluent
+
+    #echo "Generate csr"
+    keytool -keystore $path/kafka/kafka.server$i.keystore.jks -alias $ALIAS$i -certreq -file $path/kafka/kafka$i.crt -storepass confluent -keypass confluent
+
+    #echo "Sign crt"
+    openssl x509 -req -CA $path/ca.crt -CAkey $path/ca.key -in $path/kafka/kafka$i.crt -out $path/kafka/kafka$i.signed.crt -days $VALIDITY -CAcreateserial 
+
+    #echo "Add CA to keystore"
+    keytool -keystore $path/kafka.server$i.keystore.jks -alias CA -import -file $path/ca.crt -storepass confluent -keypass confluent
+
+    #echo "Add signed cert to keystore"
+    keytool -keystore $path/kafka.server$i.keystore.jks -alias $path/kafka/kafka$i.crt -import -file $path/kafka/kafka$i.signed.crt -storepass confluent -keypass confluent
+
+    #echo "Add CA to truststore"
+    keytool -keystore $path/kafka/kafka.server$i.truststore.jks -alias CA -importcert -file $path/ca.crt -storepass confluent -keypass confluent
+>>>>>>> parent of 23e6b25... Kafka is almost done, only custom hosts are needed
 done
