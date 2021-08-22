d6974fa08e1dcbd35350b4b798070c48077e27ab
XYZ <deikare@gmail.com>
2021-08-22 22:37:48 +0200

    Add statistics to timeseries

--- backend/src/main/java/com/example/backend/data/model/timeseries/DeviceBaseTimeseries.java
+++ backend/src/main/java/com/example/backend/data/model/timeseries/DeviceBaseTimeseries.java
@@ -6,22 +6,32 @@ import java.util.List;
 public class DeviceBaseTimeseries<V> {
     private final List<Point<V>> points;
 
+    private int numberOfPoints;
+
     public DeviceBaseTimeseries() {
         points = new ArrayList<>();
+        numberOfPoints = 0;
     }
+
 
     @Override
     public String toString() {
         return "DeviceBaseTimeseries{" +
                 "points=" + points +
+                ", numberOfPoints=" + numberOfPoints +
                 '}';
     }
 
     public void addPoint(Point<V> newPoint) {
         points.add(newPoint);
+        numberOfPoints++;
     }
 
     public List<Point<V>> getPoints() {
         return points;
+    }
+
+    public int getNumberOfPoints() {
+        return numberOfPoints;
     }
 }
--- backend/src/main/java/com/example/backend/data/model/timeseries/DeviceBaseTimeseriesList.java
+++ backend/src/main/java/com/example/backend/data/model/timeseries/DeviceBaseTimeseriesList.java
@@ -2,15 +2,26 @@ package com.example.backend.data.model.timeseries;
 
 import com.example.backend.data.model.timeseries.interfaces.InfluxDeviceValueInterface;
 
+import java.time.Instant;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
 public class DeviceBaseTimeseriesList<V, I extends InfluxDeviceValueInterface<V>> {
     private final HashMap<TimeseriesTags<I>, DeviceBaseTimeseries<V>> timeseriesList;
 
+    private Instant start = Instant.now();
+    private Instant end = Instant.ofEpochMilli(0);
+
+    private final int seriesAmount;
+    private final int pointsAmount;
+
     public DeviceBaseTimeseriesList(List<I> recordsList) {
         timeseriesList = new HashMap<>();
         populateTimeseriesList(recordsList);
+
+        seriesAmount = timeseriesList.size();
+        pointsAmount = recordsList.size();
     }
 
     @Override
@@ -24,8 +35,31 @@ public class DeviceBaseTimeseriesList<V, I extends InfluxDeviceValueInterface<V>
         return timeseriesList;
     }
 
+    public Instant getStart() {
+        return start;
+    }
+
+    public Instant getEnd() {
+        return end;
+    }
+
+    public int getSeriesAmount() {
+        return seriesAmount;
+    }
+
+    public int getPointsAmount() {
+        return pointsAmount;
+    }
+
     private void populateTimeseriesList(List<I> recordsList) {
         for (I record : recordsList) {
+            Instant recordTime = record.getTime();
+
+            if (end.compareTo(recordTime) < 0)
+                end = record.getTime();
+            if (start.compareTo(recordTime) > 0)
+                start = record.getTime();
+
             TimeseriesTags<I> tags = new TimeseriesTags<>(record);
 
             DeviceBaseTimeseries<V> timeseries = timeseriesList.get(tags);
