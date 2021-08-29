0776f22724d62bffda927e2967b391b7b3046d85
XYZ <deikare@gmail.com>
2021-08-29 21:59:51 +0200

    Add hub crud management

--- hub-service/app/src/main/java/com/example/hubservice/kafka/config/ConsumersConfig.java
+++ hub-service/app/src/main/java/com/example/hubservice/kafka/config/ConsumersConfig.java
@@ -3,7 +3,7 @@ package com.example.hubservice.kafka.config;
 import com.example.hubservice.kafka.record.control.hub.KafkaHubControlRecordWrapper;
 import com.example.hubservice.kafka.record.crud.KafkaHubConfigurationRecordWrapper;
 import com.example.hubservice.management.hub.model.ControlSignal;
-import com.example.hubservice.management.hub.service.implementation.hub.configuration.HubConfigurationService;
+import com.example.hubservice.management.hub.service.implementation.hub.configuration.HubManagementService;
 import com.example.hubservice.utilities.loggers.abstracts.ConfigLogger;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -26,14 +26,14 @@ import java.util.Objects;
 public class ConsumersConfig {
     private final Logger logger = LoggerFactory.getLogger(ConsumersConfig.class);
 
-    private final HubConfigurationService hubConfigurationService;
+    private final HubManagementService hubManagementService;
 
     private final String serverFQDN = "iot.switzerlandnorth.cloudapp.azure.com";
     private final String [] ports = {"9092", "9093", "9094"};
     private final String bootstrapServers = getServerListWithPorts(serverFQDN, ports);
 
-    public ConsumersConfig(HubConfigurationService hubConfigurationService) {
-        this.hubConfigurationService = hubConfigurationService;
+    public ConsumersConfig(HubManagementService hubManagementService) {
+        this.hubManagementService = hubManagementService;
     }
 
     @Bean
@@ -44,7 +44,7 @@ public class ConsumersConfig {
         result.setConsumerFactory(deviceControlConsumerFactory());
         result.setConcurrency(1);
         result.setRecordFilterStrategy(consumerRecord -> {
-            boolean filterResult = !hubConfigurationService.isControlSignalPresent(consumerRecord.key());
+            boolean filterResult = !hubManagementService.isControlSignalPresent(consumerRecord.key());
             logger.info("Filtering message: " + consumerRecord + ", discard? = " + filterResult);
             return filterResult;
         });
@@ -68,7 +68,7 @@ public class ConsumersConfig {
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "hub_configuration");
+        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubManagementService.getHubId() + "_" + "hub_configuration");
 
         properties.put("security.protocol", "SSL");
         properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
@@ -98,7 +98,7 @@ public class ConsumersConfig {
             String dataIdFromKey = consumerRecord.key();
             String dataIdFromValue = consumerRecord.value().getHub().getId();
 
-            boolean filterResult = !hubConfigurationService.isHubPresent(dataIdFromKey) && hubConfigurationService.isHubPresent(dataIdFromValue);
+            boolean filterResult = !hubManagementService.isHubPresent(dataIdFromKey) && hubManagementService.isHubPresent(dataIdFromValue);
             logger.info("Filtering message: " + consumerRecord + ", discard? = " + filterResult);
             return filterResult;
         });
@@ -122,7 +122,7 @@ public class ConsumersConfig {
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "hub_configuration");
+        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubManagementService.getHubId() + "_" + "hub_configuration");
 
         properties.put("security.protocol", "SSL");
         properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
@@ -152,7 +152,7 @@ public class ConsumersConfig {
             String dataIdFromKey = consumerRecord.key();
             String dataIdFromValue = consumerRecord.value().getObject().getId();
 
-            boolean filterResult = !hubConfigurationService.isHubPresent(dataIdFromKey) && hubConfigurationService.isHubPresent(dataIdFromValue);
+            boolean filterResult = !hubManagementService.isHubPresent(dataIdFromKey) && hubManagementService.isHubPresent(dataIdFromValue);
             logger.info("Filtering message: " + consumerRecord + ", discard? = " + filterResult);
             return filterResult;
         });
@@ -176,7 +176,7 @@ public class ConsumersConfig {
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
 
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "hub_configuration");
+        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubManagementService.getHubId() + "_" + "hub_configuration");
 
         properties.put("security.protocol", "SSL");
         properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
@@ -190,87 +190,6 @@ public class ConsumersConfig {
         ConfigLogger.produceConfigBeanCreationLog(logger, properties);
 
         ConsumerFactory<String, KafkaHubConfigurationRecordWrapper> result = new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), deserializer);
-        ConfigLogger.produceConfigBeanCreationLog(logger, result);
-
-        return result;
-    }
-
-    @Bean
-    public Map<String, Object> hubControlProperties() {
-        Map<String, Object> properties = new HashMap<>();
-
-        properties.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
-
-        properties.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-
-        properties.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "hub_control");
-
-        properties.put("security.protocol", "SSL");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "confluent");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "JKS");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.keystore.jks");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "confluent");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "JKS");
-        properties.put(org.apache.kafka.common.config.SslConfigs.SSL_KEY_PASSWORD_CONFIG, "confluent");
-
-        ConfigLogger.produceConfigBeanCreationLog(logger, properties);
-
-        return properties;
-    }
-
-    @Bean
-    public Map<String, Object> hubConfigurationProperties() {
-        Map<String, Object> result = new HashMap<>();
-
-        result.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-//        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
-//
-        result.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-
-        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "hub_configuration");
-
-        result.put("security.protocol", "SSL");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "confluent");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "JKS");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.keystore.jks");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "confluent");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "JKS");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEY_PASSWORD_CONFIG, "confluent");
-
-        ConfigLogger.produceConfigBeanCreationLog(logger, result);
-
-        return result;
-    }
-
-    @Bean
-    public Map<String, Object> deviceControlProperties() {
-        Map<String, Object> result = new HashMap<>();
-
-        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-
-//        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
-//        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
-//
-        result.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-
-        result.put(org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG, hubConfigurationService.getHubId() + "_" + "device_control");
-
-        result.put("security.protocol", "SSL");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.truststore.jks");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "confluent");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "JKS");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/home/deikare/wut/iot-system/hub-service/certs/kafka-client/kafka.client.keystore.jks");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "confluent");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "JKS");
-        result.put(org.apache.kafka.common.config.SslConfigs.SSL_KEY_PASSWORD_CONFIG, "confluent");
-
         ConfigLogger.produceConfigBeanCreationLog(logger, result);
 
         return result;
