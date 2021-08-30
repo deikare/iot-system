1cbb82e369b344c9d0bfb9593384be16d1e55ef9
XYZ <deikare@gmail.com>
2021-08-30 14:17:57 +0200

    test for dependencies:

--- backend/app/src/main/java/com/example/backend/device/manager/service/implementation/crud/DependentServiceImplementation.java
+++ backend/app/src/main/java/com/example/backend/device/manager/service/implementation/crud/DependentServiceImplementation.java
@@ -32,8 +32,9 @@ public class DependentServiceImplementation<
     @Override
     public B addDependentAndBindItToMaster(B dependent, M master) {
         dependent.setMaster(master);
-        B addedDependent = addObject(dependent);
         masterServiceImplementation.addDependentToListInObject(master, dependent);
+        B addedDependent = addObject(dependent);
+//        masterServiceImplementation.addDependentToListInObject(master, dependent);
         return addedDependent;
     }
 }
