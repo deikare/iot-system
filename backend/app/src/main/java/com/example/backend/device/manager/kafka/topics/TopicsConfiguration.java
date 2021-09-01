a3b3c4512fb99076d74456e1e35e8cf9e78eca04
XYZ <deikare@gmail.com>
2021-09-01 14:53:17 +0200

    Add topic creation to spring

---
+++ backend/app/src/main/java/com/example/backend/device/manager/kafka/topics/TopicsConfiguration.java
@@ -0,0 +1,54 @@
+package com.example.backend.device.manager.kafka.topics;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class TopicsConfiguration {
+    @Bean
+    public KafkaAdmin admin() {
+        String bootstrapServers = "kafka1:8092,kafka2:8093,kafka3:8094";
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic hubsConfigurationTopic() {
+        return TopicBuilder.name("hubs")
+                .partitions(10)
+                .replicas(3)
+                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
+                .compact()
+                .build();
+    }
+
+    @Bean
+    public NewTopic hubsControlTopic() {
+        return TopicBuilder.name("sent_hub_controls")
+                .partitions(10)
+                .replicas(3)
+                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
+                .compact()
+                .build();
+    }
+
+    @Bean
+    public NewTopic deviceControlTopic() {
+        return TopicBuilder.name("sent_device_controls")
+                .partitions(10)
+                .replicas(3)
+                .config(TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG, "2")
+                .compact()
+                .build();
+    }
+}
